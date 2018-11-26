79a9a82a8d2d72b6e7ab4414b181e4305661aefd
Arif <44020819+azimarif@users.noreply.github.com>
2018-11-26 13:29:40 +0530

    Modify failing test

--- test.js
+++ test.js
@@ -3,6 +3,6 @@ const assert = require('assert');
 describe('sum',function(){
   it('check with two no',function(){
     assert.equal(sum(1,2), 3);
-    assert.equal(sum(1,2), 4);
+    assert.equal(sum(1,3), 4);
   })
 });
