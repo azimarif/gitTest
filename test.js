3dcd3b10f01697dab2a02a421b676d9eff83bb44
Arif <44020819+azimarif@users.noreply.github.com>
2018-11-26 14:09:34 +0530

    test failed

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const {sum}= require('./lib.js');
 const assert = require('assert');
 describe('sum',function(){
   it('check with two no',function(){
-    assert.equal(sum(1,2), 3);
+    assert.equal(sum(1,2), 33);
     assert.equal(sum(1,3), 4);
   })
   it('check with two no again',function(){
